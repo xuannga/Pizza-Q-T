5e230f59f4cca0fd5602373cc5e4fdc7926886f8
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-10-14 22:23:09 -0700

    got seeds working with schema, able to seed orders, kitchen, profile

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,40 +3,31 @@ const { School, Class, Professor } = require('../models');
 const resolvers = {
   Query: {
      
-     order:  //single order - kitchen
+     order:  {},//single order - kitchen},
 
-     getorders:   // get all active orders, by status - kitchen
+     getorders:  { },// get all active orders, by status - kitchen},
 
-     getqueue:     // might be same as getorders
+     getqueue: {},    // might be same as getorders
 
-     getusers: //
+     getusers: {},//
 
-     me:    //  getmy order, history - user
+     me: {},   //  getmy order, history - user
     // schools: async () => {
     //   return await School.find({}).populate('classes').populate({
     //     path: 'classes',
     //     populate: 'professor'
     //   });
     // },
-    // classes: async () => {
-    //   return await Class.find({}).populate('professor');
-    // },
-    // class: async (parent, args) => {
-    //   return await Class.findById(args.id);
-    // },
-    // professors: async () => {
-    //   return await Professor.find({}).populate('classes');
-    // }
   },
   // Define the functions that will fulfill the mutations
   Mutation: {
-    createuser:  // byuser
-    addorder:  // by user
-    updateorder:  // user can change the order,
-    login:   // by user
-    cancelorder:  // by user
-    updatequeue: 
-    logout: // Do we need? How is accomplished
+    createuser: {} ,// byuser
+    addorder:  {},// by user
+    updateorder: {}, // user can change the order,
+    login: {},  // by user
+    cancelorder: {}, // by user
+    updatequeue: {},
+    logout: {},// Do we need? How is accomplished
     //   addSchool: async (parent, { name, location, studentCount }) => {
   //     // Create and return the new School object
   //     return await School.create({ name, location, studentCount });
