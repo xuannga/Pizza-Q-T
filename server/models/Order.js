9e89ca2a0d86a84afe9328bb43c25b645ee9399b
RSimer <65471088+RSimer@users.noreply.github.com>
2021-10-19 21:35:35 -0700

    changed up front end to look more like a pizzeria

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const Order = new Schema({
+const orderSchema = new Schema({
   purchaseDate: {
     type: Date,
     default: Date.now
