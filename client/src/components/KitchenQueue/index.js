7ede87648fdd5a1e4ed95fd6685eb21e9bef7a20
gmcmurray <83099840+gmcmurray@users.noreply.github.com>
2021-10-21 17:38:42 -0700

    Created React Table for queue data it is in KitchenQueue component

---
+++ client/src/components/KitchenQueue/index.js
@@ -0,0 +1,70 @@
+import React, {useMemo} from 'react'
+import { useTable } from 'react-table'
+ 
+import MOCK_DATA from '../../utils/mockdata.json'
+import  '../../utils/table.css'
+
+const COLUMNS = [
+  {
+      Header: 'OrderId',
+      accessor: 'orderId'
+  },
+  {
+      Header: 'Pizzas',
+      accessor: 'pizzas'
+  },
+  {
+      Header: 'Priority',
+      accessor: 'priority'
+  },
+  {
+      Header: 'Status',
+      accessor: 'status'
+  }
+
+]
+
+ const KitchenQueue = () => {
+  const columns = useMemo(()=> COLUMNS,[])
+  const data = useMemo(()=> MOCK_DATA,[])
+
+  const tableInstance = useTable({
+    columns,
+    data
+  })
+
+  const { getTableProps, getTableBodyProps,
+     headerGroups, rows, prepareRow} = tableInstance
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {
+          headerGroups.map((headerGroup) => (
+        <tr {...headerGroup.getHeaderGroupProps()}>
+          {headerGroup.headers.map((column)=> (
+          <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+          ))}
+          </tr>
+         ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row)
+          return(
+        <tr {...row.getRowProps()}>
+          {
+            row.cells.map((cell)=>{
+              return <td {...cell.getCellProps()}>
+                {cell.render('Cell')}
+              </td>
+            })}
+        </tr>
+        )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default KitchenQueue;
---
+++ client/src/utils/columns.js
@@ -0,0 +1,19 @@
+export const COLUMNS = [
+    {
+        Header: 'OrderId',
+        accessor: 'orderId'
+    },
+    {
+        Header: 'Pizzas',
+        accessor: 'pizzas'
+    },
+    {
+        Header: 'Priority',
+        accessor: 'priority'
+    },
+    {
+        Header: 'Status',
+        accessor: 'status'
+    }
+
+]
